db168a7efbadf7e6bffbcfa5edd0a3cb7dc57b9a
BL4 <clem29auv@gmail.com>
2024-12-22 17:50:26 +0100

    + | Init

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,25 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cauvray <cauvray@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/22 13:19:46 by cauvray           #+#    #+#             */
-/*   Updated: 2024/12/22 16:41:56 by cauvray          ###   ########.fr       */
+/*   Created: 2024/12/22 17:48:06 by cauvray           #+#    #+#             */
+/*   Updated: 2024/12/22 17:48:28 by cauvray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-
-int main(int argc, char *argv[])
+int	main(void)
 {
-	std::cout << "Args"
-
-	(void) argc;
-	(void) argv;
-
-	for (int i = 0; i < 5; i++)
-	{
-		std::cout
+	return (0);
 }

10d5fda49adf627ceb697bb00b3417b92e15103f
BL4 <clem29auv@gmail.com>
2024-12-22 22:27:14 +0100

    ~ | Finished cpp00 ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: cauvray <cauvray@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/22 13:19:46 by cauvray           #+#    #+#             */
-/*   Updated: 2024/12/22 16:41:56 by cauvray          ###   ########.fr       */
+/*   Updated: 2024/12/22 22:25:41 by cauvray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,22 @@
 
 int main(int argc, char *argv[])
 {
-	std::cout << "Args"
-
-	(void) argc;
-	(void) argv;
-
-	for (int i = 0; i < 5; i++)
+	if (argc <= 1)
 	{
-		std::cout
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+
+	for (int i = 1; i < argc; i++)
+	{
+		int j = 0;
+		while (argv[i][j])
+		{
+			argv[i][j] = toupper(argv[i][j]);
+			j++;
+		}
+		std::cout << argv[i];
+	}
+	std::cout << std::endl;
+	return (0);
 }

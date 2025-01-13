f6560569ea184bc8667c789b1b13bdd1be834617
cauvray <cauvray@student.42lehavre.fr>
2025-01-13 14:23:03 +0100

    ~ | Update megaphone with for loop

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,30 +6,23 @@
 /*   By: cauvray <cauvray@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/22 13:19:46 by cauvray           #+#    #+#             */
-/*   Updated: 2024/12/22 22:25:41 by cauvray          ###   ########.fr       */
+/*   Updated: 2025/01/13 14:20:25 by cauvray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main(int argc, char *argv[])
-{
-	if (argc <= 1)
-	{
+int main(int argc, char *argv[]) {
+	if (argc <= 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (0);
+		return 0;
 	}
 
-	for (int i = 1; i < argc; i++)
-	{
-		int j = 0;
-		while (argv[i][j])
-		{
+	for (int i = 1; i < argc; i++) {
+		for (int j = 0; argv[i][j]; j++)
 			argv[i][j] = toupper(argv[i][j]);
-			j++;
-		}
 		std::cout << argv[i];
 	}
 	std::cout << std::endl;
-	return (0);
+	return 0;
 }

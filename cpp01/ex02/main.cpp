68279fe7b558d4daf85290a7f231259523859372
BL4 <clem29auv@gmail.com>
2025-01-27 17:12:37 +0100

    ~ | Finished cpp01 ex02

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cauvray <cauvray@student.42lehavre.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 16:36:45 by cauvray           #+#    #+#             */
+/*   Updated: 2025/01/27 17:02:07 by cauvray          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main() {
+	std::string string = "HI THIS IS BRAIN";
+	std::string *stringPTR = &string;
+	std::string &stringREF = string;
+
+	std::cout	<< "Memory address of the string variable\t: `" << &string << "`" << std::endl
+				<< "Memory address held by stringPTR\t: `" << stringPTR << "`" << std::endl
+				<< "Memory address held by stringREF\t: `" << &stringREF << "`" << std::endl;
+
+	std::cout	<< std::endl;
+
+	std::cout	<< "Value of the string variable\t: `" << string << "`" << std::endl
+				<< "Value pointed to by stringPTR\t: `" << *stringPTR << "`" << std::endl
+				<< "Value pointed to by stringREF\t: `" << stringREF << "`" << std::endl;
+}

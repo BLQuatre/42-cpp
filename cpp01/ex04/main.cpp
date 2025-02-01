8e6975bdec827410c0af8f1484af1de4c27ebc58
BL4 <clem29auv@gmail.com>
2025-02-01 19:41:54 +0100

    + | Finished cpp01 ex04

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cauvray <cauvray@student.42lehavre.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 16:36:45 by cauvray           #+#    #+#             */
+/*   Updated: 2025/02/01 19:35:52 by cauvray          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+
+bool	is_args_valid(int argc, char *argv[]) {
+	if (argc != 4) {
+		std::cout << "usage: " << argv[0] << " <file> <replace_from> <replace_to>" << std::endl;
+		return false;
+	}
+
+	if (*(argv[1]) == '\0') {
+		std::cout << "error: file must be at least one char long" << std::endl;
+		return false;
+	}
+
+	if (*(argv[2]) == '\0') {
+		std::cout << "error: replace_from must be at least one char long" << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+bool	replace_line(std::string fileName, std::string replaceFrom, std::string replaceTo) {
+	std::ifstream read_file(fileName.c_str());
+	if (!read_file.is_open()) {
+		std::cout << "error: " << fileName << ": Unable to open or read file" << std::endl;
+		return false;
+	}
+
+	std::string replaceFileName = fileName.append(".replace");
+	std::ofstream write_file(replaceFileName.c_str());
+	if (!write_file.is_open()) {
+		std::cout << "error: " << replaceFileName << ": Unable to open or write file" << std::endl;
+		read_file.close();
+		return false;
+	}
+	write_file.flush();
+
+	std::string line;
+	while (std::getline(read_file, line))
+	{
+		std::size_t found = 0;
+		std::size_t last_found = 0;
+		while (true) {
+			found = line.find(replaceFrom, last_found);
+			write_file << line.substr(last_found, found - last_found);
+			if (found == std::string::npos)
+				break;
+			write_file << replaceTo;
+			if (replaceFrom.length() > 0)
+				last_found = found + replaceFrom.length();
+			else
+				last_found = found + 1;
+		}
+		write_file << std::endl;
+	}
+
+	read_file.close();
+	write_file.close();
+	return true;
+}
+
+int	main(int argc, char *argv[]) {
+	if (!is_args_valid(argc, argv))
+		return EXIT_FAILURE;
+
+	bool result = replace_line(argv[1], argv[2], argv[3]);
+	if (!result)
+		return EXIT_FAILURE;
+	return EXIT_SUCCESS;
+}

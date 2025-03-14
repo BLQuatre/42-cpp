60544dd9e4e859c076a1bf19c5e7e3e53c3b8696
BL4 <clem29auv@gmail.com>
2025-03-14 22:03:16 +0100

    ~ | Finished cpp09 ex01

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -6,14 +6,85 @@
 /*   By: cauvray <cauvray@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 21:22:14 by cauvray           #+#    #+#             */
-/*   Updated: 2025/03/10 21:51:18 by cauvray          ###   ########.fr       */
+/*   Updated: 2025/03/14 22:02:30 by cauvray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <cstdlib>
 #include <iostream>
+#include <stack>
 #include "RPN.hpp"
 
-int	RPN::calculate(const std::string input) {
-	std::cout << "Calculating: `" << input << "`" << std::endl;
-	return 0;
+static bool	isop(int c) {
+	return (c == '+' || c == '-' || c == '*' || c == '/');
+}
+
+static bool isValidExpression(const std::string &expression) {
+	std::string::const_iterator it = expression.begin();
+	int elements = 0;
+
+	while (it != expression.end()) {
+		if (!isop(*it) && !isdigit(*it))
+		return false;
+
+		if (isop(*it)) {
+			if (elements < 2)
+				return false;
+			elements--;
+		} else
+			elements++;
+
+		it++;
+		if (it != expression.end()) {
+			if (*it != ' ')
+				return false;
+			it++;
+			if (it == expression.end())
+				return false;
+		}
+	}
+
+	return (elements == 1);
+}
+
+static void	doOperation(std::stack<int> &numbers, int op) {
+	int i1 = numbers.top();
+	numbers.pop();
+	int i2 = numbers.top();
+	numbers.pop();
+
+	int result = 0;
+
+	switch (op) {
+		case '+':
+			result = i2 + i1;
+			break;
+		case '-':
+			result = i2 - i1;
+			break;
+		case '*':
+			result = i2 * i1;
+			break;
+		case '/':
+			result = i2 / i1;
+			break;
+	}
+
+	numbers.push(result);
+}
+
+int	RPN::calculate(std::string input) {
+	if (!isValidExpression(input)) {
+		std::cerr << "Error" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	std::stack<int> numbers;
+	for (std::string::const_iterator it = input.begin(); it != input.end(); it++) {
+		if (isdigit(*it))
+			numbers.push(std::atoi(it.base()));
+		else if (isop(*it))
+			doOperation(numbers, *it);
+	}
+	return numbers.top();
 }
--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -6,33 +6,24 @@
 /*   By: cauvray <cauvray@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 00:18:03 by cauvray           #+#    #+#             */
-/*   Updated: 2025/03/10 22:03:18 by cauvray          ###   ########.fr       */
+/*   Updated: 2025/03/14 22:02:10 by cauvray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cstdlib>
 #include <iostream>
-#include "RPN.hpp"
 #include <stack>
+#include "RPN.hpp"
 
-int main() {
-	// int i = RPN::calculate("coucou");
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		std::cerr << "Error" << std::endl;
+		return EXIT_FAILURE;
+	}
 
-	// std::cout << "Result: " << i << std::endl;
+	int result = RPN::calculate(argv[1]);
 
-
-	std::stack<std::string> test;
-	test.push("3");
-	test.push("2");
-	test.push("1");
-	std::string s1 = test.top();
-	test.pop();
-	std::string s2 = test.top();
-	test.pop();
-	int result = std::atoi(s1.c_str()) + std::atoi(s2.c_str());
-
-
-
+	std::cout << result << std::endl;
 
 	return EXIT_SUCCESS;
 }

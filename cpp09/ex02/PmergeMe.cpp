5d332081b4c187c8cd4264948af9ee0459f5ee05
BL4 <cauvray@student.42lehavre.fr>
2025-03-27 21:16:16 +0100

    ~ | Make constructor etc private for class PmergeMe

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -6,28 +6,8 @@
 /*   By: cauvray <cauvray@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 21:22:14 by cauvray           #+#    #+#             */
-/*   Updated: 2025/03/09 21:42:56 by cauvray          ###   ########.fr       */
+/*   Updated: 2025/03/27 19:44:27 by cauvray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
 #include "PmergeMe.hpp"
-
-PmergeMe::PmergeMe() {
-	std::cout << "Default constructor called" << std::endl;
-}
-
-PmergeMe::PmergeMe(const PmergeMe &pmergeMe) {
-	std::cout << "Copy constructor called" << std::endl;
-	*this = pmergeMe;
-}
-
-PmergeMe::~PmergeMe() {
-	std::cout << "Destructor called" << std::endl;
-}
-
-PmergeMe	&PmergeMe::operator=(const PmergeMe &pmergeMe) {
-	(void) pmergeMe;
-	std::cout << "Copy assignment operator called" << std::endl;
-	return *this;
-}
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: cauvray <cauvray@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 21:21:48 by cauvray           #+#    #+#             */
-/*   Updated: 2025/03/09 21:41:10 by cauvray          ###   ########.fr       */
+/*   Updated: 2025/03/27 19:44:15 by cauvray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 
 class PmergeMe {
 	private:
-
-	public:
 		PmergeMe();
 		PmergeMe(const PmergeMe &pmergeMe);
 		PmergeMe	&operator=(const PmergeMe &pmergeMe);

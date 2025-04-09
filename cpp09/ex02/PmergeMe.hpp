c650ddde9c8729f1ebc25118a21bc79ea90503cb
BL4 <clem29auv@gmail.com>
2025-04-09 22:43:37 +0200

    ~ | Finished cpp09 ex02

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,19 +6,153 @@
 /*   By: cauvray <cauvray@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 21:21:48 by cauvray           #+#    #+#             */
-/*   Updated: 2025/03/27 19:44:15 by cauvray          ###   ########.fr       */
+/*   Updated: 2025/04/09 16:29:32 by cauvray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PMERGE_ME_HPP
-# define PMERGE_ME_HPP
+#pragma once
+#include <vector>
+#include <algorithm>
+#include <iostream>
 
+template <typename Container>
 class PmergeMe {
 	private:
+		typedef typename Container::value_type ValueType;
+		typedef typename std::vector<ValueType>::iterator Iterator;
+		Container& container;
+
 		PmergeMe();
 		PmergeMe(const PmergeMe &pmergeMe);
 		PmergeMe	&operator=(const PmergeMe &pmergeMe);
+
+		std::vector<size_t>	generateJacobsthalSequence(size_t max_n);
+		std::vector<size_t>	generateInsertionOrder(size_t m);
+		void				fordJohnsonSort(size_t start, size_t end);
+
+	public:
+		PmergeMe(Container& c);
 		~PmergeMe();
+
+		void sort();
+		void printContainer();
 };
 
-#endif
+template <typename Container>
+PmergeMe<Container>::PmergeMe(Container& c) : container(c) {}
+
+template <typename Container>
+PmergeMe<Container>::~PmergeMe() {}
+
+template <typename Container>
+void PmergeMe<Container>::sort() {
+	fordJohnsonSort(0, this->container.size());
+}
+
+template <typename Container>
+void PmergeMe<Container>::printContainer() {
+	typename Container::const_iterator it = this->container.begin();
+	while (it != this->container.end()) {
+		std::cout << *it << " ";
+		++it;
+	}
+	std::cout << std::endl;
+}
+
+/**
+ * @note J(0) = 0, J(1) = 1, J(n) = J(n-1) + 2 * J(n-2)
+ */
+template <typename Container>
+std::vector<size_t>	PmergeMe<Container>::generateJacobsthalSequence(size_t max_n) {
+	std::vector<size_t> jacob;
+	jacob.push_back(0);
+	jacob.push_back(1);
+	while (jacob.back() <= max_n) {
+		size_t next = jacob[jacob.size() - 1] + 2 * jacob[jacob.size() - 2];
+		jacob.push_back(next);
+	}
+	return jacob;
+}
+
+template <typename Container>
+std::vector<size_t>	PmergeMe<Container>::generateInsertionOrder(size_t m) {
+	std::vector<size_t> order;
+	if (m == 0)
+		return order;
+	order.push_back(0);
+	if (m == 1)
+		return order;
+
+	std::vector<bool> added(m, false);
+	added[0] = true;
+
+	std::vector<size_t> jacob = this->generateJacobsthalSequence(m);
+	for (size_t i = 2; i < jacob.size(); ++i) {
+		size_t current = std::min(jacob[i], m - 1);
+		size_t prev = jacob[i - 1];
+		if (current <= prev)
+			continue;
+
+		for (size_t j = current; j > prev; --j) {
+			if (!added[j]) {
+				order.push_back(j);
+				added[j] = true;
+			}
+			if (j == 0)
+				break;
+		}
+	}
+
+	for (size_t j = m - 1; j >= 1; --j) {
+		if (!added[j]) {
+			order.push_back(j);
+			added[j] = true;
+		}
+	}
+
+	return order;
+}
+
+template <typename Container>
+void	PmergeMe<Container>::fordJohnsonSort(size_t start, size_t end) {
+	size_t size = end - start;
+	if (size <= 1) return;
+
+	bool has_pending = (size % 2 != 0);
+	ValueType pending;
+	size_t pending_pos = end - 1;
+	if (has_pending) {
+		pending = this->container[pending_pos];
+		end--;
+	}
+
+	for (size_t i = start; i < end; i += 2) {
+		if (this->container[i] > this->container[i + 1]) {
+			std::swap(this->container[i], this->container[i + 1]);
+		}
+	}
+
+	std::vector<ValueType> high, low;
+	for (size_t i = start + 1; i < end; i += 2) {
+		high.push_back(this->container[i]);
+		low.push_back(this->container[i - 1]);
+	}
+
+	PmergeMe<std::vector<ValueType> >(high).sort();
+
+	std::vector<size_t> insert_order = this->generateInsertionOrder(low.size());
+	for (size_t k = 0; k < insert_order.size(); ++k) {
+		size_t idx = insert_order[k];
+		Iterator it = std::lower_bound(high.begin(), high.end(), low[idx]);
+		high.insert(it, low[idx]);
+	}
+
+	if (has_pending) {
+		Iterator it = std::lower_bound(high.begin(), high.end(), pending);
+		high.insert(it, pending);
+	}
+
+	for (size_t i = 0; i < high.size(); ++i) {
+		this->container[start + i] = high[i];
+	}
+}
--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,13 +6,67 @@
 /*   By: cauvray <cauvray@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 00:18:03 by cauvray           #+#    #+#             */
-/*   Updated: 2025/03/09 21:41:56 by cauvray          ###   ########.fr       */
+/*   Updated: 2025/04/09 22:42:42 by cauvray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cstdlib>
+#include <sstream>
+#include <deque>
+#include <ctime>
 #include "PmergeMe.hpp"
 
-int main() {
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		std::cerr << "Error: " << argv[0] << " <numbers...>" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::vector<int> inputVec;
+	std::deque<int> inputDeque;
+	for (int i = 1; i < argc; ++i) {
+		std::stringstream ss(argv[i]);
+		int val;
+		if (ss >> val && val > 0) {
+			inputVec.push_back(val);
+			inputDeque.push_back(val);
+		} else {
+			std::cerr << "Error: " << argv[i] << " is not a valid positive integer." << std::endl;
+			return EXIT_FAILURE;
+		}
+	}
+
+	std::cout << "WITH VECTOR" << std::endl;
+	PmergeMe<std::vector<int> > pmergeMeVector(inputVec);
+
+	std::cout << "Before:\t";
+	pmergeMeVector.printContainer();
+
+	std::clock_t startVector = std::clock();
+	pmergeMeVector.sort();
+	std::clock_t endVector = std::clock();
+	double elapsedVector = endVector - startVector;
+
+	std::cout << "After:\t";
+	pmergeMeVector.printContainer();
+
+
+	std::cout << "WITH DEQUE" << std::endl;
+	PmergeMe<std::deque<int> > pmergeMeDeque(inputDeque);
+
+	std::cout << "Before:\t";
+	pmergeMeDeque.printContainer();
+
+	std::clock_t startDeque = std::clock();
+	pmergeMeDeque.sort();
+	std::clock_t endDeque = std::clock();
+	double elapsedDeque = endDeque - startDeque;
+
+	std::cout << "After:\t";
+	pmergeMeDeque.printContainer();
+
+	std::cout << "Time to process a range of " << inputVec.size() << " elements with std::vector\t: " << elapsedVector << " µs" << std::endl;
+	std::cout << "Time to process a range of " << inputDeque.size() << " elements with std::deque\t: " << elapsedDeque << " µs" << std::endl;
+
 	return EXIT_SUCCESS;
 }
